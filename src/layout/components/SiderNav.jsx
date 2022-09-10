1877a0675689d7cf300406071d900904d1d0ffaa
huanglin <2762565371@qq.com>
2022-09-10 16:36:12 +0800

    添加菜单伸缩功能

--- src/layout/components/SiderNav.jsx
+++ src/layout/components/SiderNav.jsx
@@ -4,16 +4,19 @@ import { useNavigate, useLocation } from 'react-router-dom'
 import { Layout } from 'antd'
 import menu  from '../../menu'
 import useParentRoute from '../../hooks/useParentRoute'
+import LogoSvg from '../../icons/svg/logo.svg'
 
 const { Sider } = Layout
 
 const titleStyle = {
-	color: 'lightblue', 
-	height: '64px',
-	lineHeight: '64px',
+	height: '48px',
+	lineHeight: '48px',
 	fontWeight: '600', 
+	fontSize: '20px',
 	fontFamily: '楷体', 
-	textAlign: 'center'
+	color: 'lightblue',
+	textAlign: 'center',
+	overflow: 'hidden'
 }
 
 const SiderNav = () => {
@@ -22,9 +25,11 @@ const SiderNav = () => {
 	// 当前路由
 	const location = useLocation()
 
+	const [menuCollape, setMenuCollape] = useState(false)
 	const [current, setCurrent] = useState(location.pathname)
 
 	const [parent] = useParentRoute(current)
+
 
 	
 	const onMenu = event => {
@@ -33,21 +38,26 @@ const SiderNav = () => {
 	}
 
 	return (
-		<Sider style={{width: 'auto'}} className='layout-sider'>
-			<h2 style={titleStyle}>
-				弄因农产品后台
+		<Sider
+			collapsible
+			collapsed={menuCollape}
+			onCollapse={value => setMenuCollape(value)}
+		>
+			<div>
+				<p style={titleStyle}>
+					{menuCollape ? <img style={{marginLeft: '6px'}} src={LogoSvg} alt=""/> : '弄因农产品后台'}
+				</p>
 				<>
 					<Menu
 						theme={'dark'}
 						onClick={onMenu}
 						mode={"inline"}
 						items={menu}
-						inlineCollapsed={false}
 						defaultOpenKeys={[parent]}
 						defaultSelectedKeys={[current]}
 					/>
 				</>
-			</h2>
+			</div>
 		</Sider>
 	)
 }
